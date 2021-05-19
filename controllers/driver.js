8baabe23310575421727500466592d90fa1fccb3
brianwachira <brianwachira7@gmail.com>
2021-05-20 00:28:26 +0300

    [FEATURE]: driver endpoints

---
+++ controllers/driver.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+const driverRouter = require('express').Router()
+const Driver = require('../models/driver')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
+
+driverRouter.post('/', async (request, response) => {
+    const body = request.body
+    const token = getTokenFrom
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if(!token || !decodedToken.email) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const driver = new Driver({
+        name : body.name,
+        phone : body.phone,
+    })
+
+    const savedDriver = await driver.save(
+    
+    response.json(savedDriver)
+    )
+})
+
+module.exports = driverRouter
