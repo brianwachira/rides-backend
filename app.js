5512b95fbdd5f0cdf6d6c501b9ea86e524f302d8
brianwachira <brianwachira7@gmail.com>
2021-05-19 23:25:06 +0300

    [Feature]: initialize system

---
+++ app.js
@@ -0,0 +1,31 @@
+const config = require('./utils/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+require('express-async-errors')
+const cors = require('cors')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
