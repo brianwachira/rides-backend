1cee0299606add8ce1e40d06a5c644606d644248
brianwachira <brianwachira7@gmail.com>
2021-05-20 00:18:30 +0300

    add driverRouter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 require('express-async-errors')
 const cors = require('cors')
 const loginRouter = require('./controllers/login')
+const driverRouter = require('./controllers/driver')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/login', loginRouter)
+app.use('/driver', driverRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
