32dc135dca9bae2aa27221b6273252317477d4bc
brianwachira <brianwachira7@gmail.com>
2021-05-20 00:29:09 +0300

    [FEATURE] : add mongoDB models

---
+++ models/driver.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const driverSchema = mongoose.Schema({
+    name: {
+        type: String,
+        
+    },
+    phone: String,
+    suspended: {
+        type: Boolean,
+        default: 'false'
+    },
+    rides: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Ride'
+        }
+    ]
+})
+
+driverSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Driver = mongoose.model('Driver', driverSchema)
+
+module.exports = Driver
