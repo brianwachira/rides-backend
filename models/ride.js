5f139f7e938956a3072d5a3df9e425e3fce74f22
brianwachira <brianwachira7@gmail.com>
2021-05-20 14:46:24 +0300

    [FEATURE]: models for ride

---
+++ models/ride.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const rideSchema = mongoose.Schema({
+    pickupPoint: {
+        type: [number],
+        required: true
+    },
+    destinationPoint: {
+        type: [number],
+        required: true
+    },
+    status: {
+        type : String,
+        required: true
+    },
+    passenger: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Passenger'
+    },
+    driver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Driver'
+    },
+})
+
+rideSchema.plugin(uniqueValidator)
+
+rideSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Ride = mongoose.model('Ride', rideSchema)
+
+module.exports = Ride
